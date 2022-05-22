107b1ae12033255c5f0fab4ac7f103bf4a148129
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2022-05-22 21:05:02 +0700

    lab8: using delegate

---
+++ Labs/Lab8Delegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab8Delegate
+{
+    class Program
+    {
+        //Define delegate function
+        public delegate void DelegateShowMessage(string message);
+
+        static void Main(string[] args)
+        {
+            //Instantiate the delegate.
+            DelegateShowMessage showMessage = ShowMessage;           
+            //Call delegate
+            showMessage("Call delegate");
+
+            //Call deleaget from function callback
+            MethodWithCallback("Call delegate by callback!!!", showMessage);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Function calculation for delegate
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Method callback using for delegate
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="calculation"></param>
+        static void MethodWithCallback(string message, DelegateShowMessage showMessage)
+        {
+            showMessage(message);
+        }
+    }
+}
